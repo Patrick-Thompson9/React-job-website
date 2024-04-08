4c0887b5c556c11d2cd9dc443f1490175e0f916a
PatrickThompson9 <patrick@aoms-tech.com>
2024-04-08 00:17:35 -0300

    added not found page

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,14 @@ import {
 import JobListing from "./components/JobListing";
 import HomePage from "./pages/HomePage";
 import MainLayout from "./layouts/MainLayout";
+import NotFoundPage from "./pages/NotFoundPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path="/jobs" element={<JobListing numJobs={20} />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Route>
   )
 );
