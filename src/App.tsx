fafd6bc0d882031090e3e34105ce76e0d42095be
PatrickThompson9 <patrick@aoms-tech.com>
2024-04-07 22:59:24 -0300

    made view all jobs component

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,19 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import HomeCards from "./components/HomeCards";
 import JobListing from "./components/JobListing";
+import ViewAllJobs from "./components/ViewAllJobs";
 
 function App() {
   return (
     <>
       <Navbar />
-
       <Hero
         title="Become a React Dev"
         subtitle="Find the React job that fits your needs and skills!"
       />
-
       <HomeCards />
-
       <JobListing />
-
-      <section className="m-auto max-w-lg my-10 px-6">
-        <a
-          href="jobs.html"
-          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >
-          View All Jobs
-        </a>
-      </section>
+      <ViewAllJobs />
     </>
   );
 }
