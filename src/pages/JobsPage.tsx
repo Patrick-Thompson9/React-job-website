aa960619dfc512ece262077a2849baa9df559945
PatrickThompson9 <patrick@aoms-tech.com>
2024-04-08 23:11:49 -0300

    removed numJobs props call and redundant ViewAllJobs call

--- src/pages/JobsPage.tsx
+++ src/pages/JobsPage.tsx
@@ -1,12 +1,10 @@
 import { Outlet } from "react-router-dom";
-import ViewAllJobs from "../components/ViewAllJobs";
 import JobListing from "../components/JobListing";
 
 function JobsPage() {
   return (
     <>
-      <JobListing numJobs={20} />
-      <ViewAllJobs />
+      <JobListing />
       <Outlet />
     </>
   );
