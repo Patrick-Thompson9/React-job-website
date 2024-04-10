bf8a6d12b0226fa675045b16ffbdad41a1bcd0ce
PatrickThompson9 <patrick@aoms-tech.com>
2024-04-09 21:51:48 -0300

    Made job page properly load the job with spinner

--- src/pages/JobPage.tsx
+++ src/pages/JobPage.tsx
@@ -1,5 +1,24 @@
+import { useState, useEffect } from "react";
+import JobPost, { Job } from "../components/JobPost";
+import { useParams } from "react-router-dom";
+import Spinner from "../components/Spinner";
+
 function JobPage() {
-  return <div>JobPage</div>;
+  const [currentJob, setCurrentJob] = useState<Job | null>(null);
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetch(`/api/jobs/${id}`)
+      .then((res) => res.json())
+      .then((data) => setCurrentJob(data))
+      .catch((err) => console.error(err));
+  }, []);
+
+  if (!currentJob) {
+    return <Spinner loading={!currentJob} />;
+  }
+
+  return <JobPost job={currentJob} />;
 }
 
 export default JobPage;
