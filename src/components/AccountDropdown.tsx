8f8c8c26c287e4b66811da36ad85a236421dabfb
PatrickThompson9 <patrick@aoms-tech.com>
2024-04-11 23:40:30 -0300

    brought drop down window to front

--- src/components/AccountDropdown.tsx
+++ src/components/AccountDropdown.tsx
@@ -5,7 +5,7 @@ function AccountDropdown() {
     <ul
       id="account-dropdown"
       className="text-2xl relative right-40 bottom-5 bg-white border-2 border-black rounded-md w-60
-    divide-y divide-slate-200"
+    divide-y divide-slate-200 z-50"
     >
       <NavLink to="/login">
         <li className="m-2 p-1 hover:bg-gray-300 rounded-md">Login</li>
